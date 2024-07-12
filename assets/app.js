1ca9d5d096e4b105618269875a01e8ad9fe6e54c
Zoe Heathcote <zoe08@hotmail.co.uk>
2024-07-12 19:43:16 +0000

    feat:added notes to start js file

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// Game Play:
+// board
+// pieces with numbers
+// empty square
+// game starts with shuffled array pieces
+// onclick, piece moves (side to side, or up and down)
+// pieces can only move into an adjacent tile if it is empty
+// timer begins on first click
+// game ends when pieces hit their allocated slots
+// timers ends and score is given?
+
+var piece = document.getElementsByClassName("piece")
+var piece16 = document.getElementsByClassName("piece16")
+
